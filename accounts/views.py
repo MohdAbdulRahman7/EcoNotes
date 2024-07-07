e6fb6d7820d16e04a3de2653e80ccf90853ebb66
MohdAbdulRahman7 <rahman77mohd@gmail.com>
2024-07-07 16:48:16 -0400

    accounts -- signup logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def signup_view(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        # request.POST here is the data flowing from UI to this function
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the form data to the database
+            # Log User In
+            return redirect('blogs:blogs_list')  # appname: named url value
+    else:  # Get Method Called
+        form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
-
-
